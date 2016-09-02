7fc1950bf17b6c2917aa295a190c665589252742
abarre <anthony.barre87@gmail.com>
2016-09-02 13:12:09 +0200

    [fasterize/FasterizeEngine#1184] Add test demonstrating the order difference.

---
+++ tests/assets/js_defer_order_createElement.js
@@ -0,0 +1,4 @@
+var D = document.createElement("script");
+D.setAttribute("src", "../assets/js_defer_order_createElement4.js"),
+D.setAttribute("type", "text/javascript"),
+document.getElementsByTagName("head")[0].appendChild(D)
