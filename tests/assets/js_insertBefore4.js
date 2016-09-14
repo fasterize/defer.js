bd4be65e98a9f69739188a6e4bd340cd07844657
abarre <anthony.barre87@gmail.com>
2016-09-14 16:02:39 +0200

    [fasterize/FasterizeEngine#1184] Defer the dynamically inserted scripts.

---
+++ tests/assets/js_insertBefore4.js
@@ -0,0 +1,4 @@
+var D = document.createElement("script");
+D.setAttribute("src", "../assets/js_push4.js"),
+D.setAttribute("type", "text/javascript"),
+document.getElementsByTagName("head")[0].parentNode.insertBefore(D, document.getElementsByTagName("head")[0])
