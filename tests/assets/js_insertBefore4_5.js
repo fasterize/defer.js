353de6d7117dbd6842f63fde4b0a01248bdec80f
abarre <anthony.barre87@gmail.com>
2017-05-19 12:34:09 +0200

    [fasterize/FasterizeEngine#1394] Fallback to insertion on the first script

---
+++ tests/assets/js_insertBefore4_5.js
@@ -0,0 +1,11 @@
+var D = document.createElement("script");
+D.setAttribute("src", "../assets/js_push4.js");
+D.setAttribute("type", "text/javascript");
+D.async=1;
+document.getElementsByTagName("head")[0].parentNode.insertBefore(D, document.getElementsByTagName("head")[0])
+
+var F = document.createElement("script");
+F.setAttribute("src", "../assets/js_push5.js");
+F.setAttribute("type", "text/javascript");
+F.async=1;
+document.getElementsByTagName("head")[0].parentNode.insertBefore(F, document.getElementsByTagName("head")[0])
