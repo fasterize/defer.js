dc1d5f0b2a8b296a53ac33a37c0b1486d955e01c
abarre <anthony.barre87@gmail.com>
2017-09-08 17:05:31 +0200

    [fasterize/FasterizeEngine#1479] Fix deferjs overwriting of insertBefore.

--- tests/assets/js_insertBefore4.js
+++ tests/assets/js_insertBefore4.js
@@ -1,5 +1,15 @@
-var D = document.createElement("script");
-D.setAttribute("src", "../assets/js_push4.js");
-D.setAttribute("type", "text/javascript");
+var script = document.createElement("script");
+script.setAttribute("src", "../assets/js_push4.js");
+script.setAttribute("type", "text/javascript");
 var node = document.getElementsByTagName("head")[0].children[0]; // #will-be-removed
-node.parentNode.insertBefore(D, node);
+var insertedElement = node.parentNode.insertBefore(script, node);
+if (insertedElement != script) {
+  document.writeln("inserted script should be returned even if the insertion is delayed");
+  throw "inserted element should be returned";
+}
+var div = document.createElement("div");
+var insertedElement = node.parentNode.insertBefore(div, node);
+if (insertedElement != div) {
+  document.writeln("inserted div should be returned");
+  throw "inserted element should be returned";
+}
