9db45e06f7184c3dce100d4fe749f53ea61e1fe3
abarre <anthony.barre87@gmail.com>
2017-09-19 16:20:37 +0200

    [fasterize/FasterizeEngine#1483] Fix js minification
    - update uglifyjs and activate mangle-props and compress.
    - ensure that tests pass on the prod build.

--- fasterize_build.sh
+++ fasterize_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./node_modules/.bin/uglifyjs -m --screw-ie8 --no-copyright lib/js_defer.js > lib/js_defer-min.js
+./node_modules/.bin/uglifyjs --compress --mangle --mangle-props reserved=['psaFunc','fasterizeNs','deferJs','logs'] -- lib/js_defer.js > lib/js_defer-min.js
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = require('fs').readFileSync(__dirname + '/lib/js_defer-min.js');
+module.exports.script = require('fs').readFileSync(__dirname + '/lib/js_defer-min.js', 'utf-8');
+module.exports.scriptDebug = require('fs').readFileSync(__dirname + '/lib/js_defer.js', 'utf-8');
