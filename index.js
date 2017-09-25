bd9dc9d9698b94fa874709c3e1fc2a29f2ae9fb6
abarre <anthony.barre87@gmail.com>
2017-09-25 16:34:11 +0200

    [fasterize/FasterizeEngine#1483] Prepare scripts for export

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-module.exports.script = require('fs').readFileSync(__dirname + '/lib/js_defer-min.js', 'utf-8');
-module.exports.scriptDebug = require('fs').readFileSync(__dirname + '/lib/js_defer.js', 'utf-8');
+var crypto = require('crypto');
+var fs = require('fs');
+
+var scriptProd = fs.readFileSync(__dirname + '/lib/js_defer-min.js', 'utf-8');
+var md5Prod = crypto.createHash('md5').update(scriptProd).digest("hex");
+
+var scriptDebug = fs.readFileSync(__dirname + '/lib/js_defer.js', 'utf-8');
+var md5Debug = crypto.createHash('md5').update(scriptDebug).digest("hex");
+
+module.exports = {
+  debug: {
+    script: scriptDebug,
+    md5: md5Debug
+  },
+  prod: {
+    script: scriptProd,
+    md5: md5Prod
+  }
+};
